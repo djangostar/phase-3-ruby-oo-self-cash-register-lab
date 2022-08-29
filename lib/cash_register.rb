2d599cc5e10562947f1d4c90ac6e11d89e7525a3
Juan-Sergio Khoshdel <jsergiofm85@gmail.com>
2022-08-28 20:46:57 -0700

    commit tree c2a9d9b44b6bcbd242b0c109376cd6b615653c06

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,33 +1,31 @@
 class CashRegister
-    attr_accessor :items, :total, :discount, :last_transaction
+  attr_accessor :items, :total, :discount, :last_transaction 
 
-    def initialize(discount=0)
-        @total = 0
-        @discount = discount
-        @items = []
+  def initialize(discount=0)
+    @total = 0
+    @discount = discount
+    @items = []
+  end
+  
+  def add_item(title, price, quantity=1)
+    self.last_transaction = quantity * price
+    self.total += self.last_transaction
+    quantity.times do
+      @items << title
     end
-    
-    def add_item(title, price, quantity=1)
-        self.last_transaction = price * quantity
-        self.total += self.last_transaction
-        quantity.times do
-            items << title
-        end
-    end
+  end
 
-    def apply_discount
-        if self.discount != 0
-            discount_to_float = (100.0 - self.discount.to_f) / 100
-            self.total = (self.total * discount_to_float).to_i
-            "After the discount, the total comes to $#{self.total}."
-        else
-            "There is no discount to apply."
-        end
+  def apply_discount
+    if self.discount != 0
+      discount_to_float = (100.0 - self.discount.to_f) / 100
+      self.total = (self.total * discount_to_float).to_i
+      "After the discount, the total comes to $#{self.total}."
+    else
+      "There is no discount to apply."
     end
+  end
 
-    def void_last_transaction
-        self.total  -= self.last_transaction
-
-    end
+  def void_last_transaction
+    self.total -= self.last_transaction
+  end
 end
-
